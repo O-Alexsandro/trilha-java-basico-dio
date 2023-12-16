5970f9ef83d0ec776bef4d8ddc89f8b90639dcaf
Alexsandro Oliveira <alexsandrooliveiraribas@gmail.com>
2023-12-16 15:39:32 -0300

    Conta Terminal finalizada

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,30 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class ContaTerminal {
+
+    int numero;
+    String agencia;
+    String nomeCliente;
+    double saldo;    
+
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+    
+        System.out.println("Insira o seu número número:");
+        int numero = scanner.nextInt();
+
+        System.out.println("Por favor, digite o número da Agência:");
+        String agencia = scanner.next();
+        
+        System.out.println("Por favor, digite o seu nome:");
+        String nomeCliente = scanner.next();
+
+        System.out.println("Por favor, digite o seu saldo:");
+        double saldo = scanner.nextDouble();
+
+        System.err.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é: " + agencia + ", sua conta é: " + numero + " e seu saldo é: " + saldo + "e já está disponível para saque");
+
     }
 }
