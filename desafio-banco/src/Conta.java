2dccd08f851456fae1ab2ccc97f3c7ccffd2a4d4
O-Alexsandro <ALEXSANDROOLIVEIRARIBAS@GMAIL.COM>
2024-01-25 12:36:51 -0300

    Ajustes nas classes

---
+++ desafio-banco/src/Conta.java
@@ -0,0 +1,51 @@
+public abstract class Conta implements Iconta{
+
+    private static final int AGENCIA_PADRAO = 1;
+    private static int SEQUENCIAL = 1;
+
+    protected int agencia;
+    protected int numero;
+    protected double saldo;
+   
+
+    public Conta(){
+        this.agencia = Conta.AGENCIA_PADRAO;
+        this.numero = SEQUENCIAL++;
+    }
+   
+    public void sacar(){
+    }
+    public void depositar(){
+    }
+
+    public void transferir (){
+    }
+
+    public int getAgencia() {
+        return agencia;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    @Override
+    public void sacar(double valor) {
+
+    }
+
+    @Override
+    public void depositar(double valor) {
+
+    }
+
+    @Override
+    public void transferir(double valor, Conta ContaDestino) {
+
+    }
+}
+
