6d15930d7dba6d088492bffc0042c6267d456127
O-Alexsandro <ALEXSANDROOLIVEIRARIBAS@GMAIL.COM>
2024-01-25 11:37:17 -0300

    Criando as classes do desafio

---
+++ desafio-conta/src/main/java/org/example/Conta.java
@@ -0,0 +1,42 @@
+package org.example;
+
+public abstract class Conta implements Iconta {
+    private int agencia;
+    private int numero;
+    private double saldo;
+
+    public void sacar(){
+    }
+    public void depositar(){
+    }
+
+    public void transferir (){
+    }
+
+    public int getAgencia() {
+        return agencia;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    @Override
+    public void sacar(double valor) {
+
+    }
+
+    @Override
+    public void depositar(double valor) {
+
+    }
+
+    @Override
+    public void transferir(double valor, Conta ContaDestino) {
+
+    }
+}
