989388b1e7c00869e68e9c63a1c2725a2a1560e3
O-Alexsandro <ALEXSANDROOLIVEIRARIBAS@GMAIL.COM>
2024-01-27 14:32:48 -0300

    Pilhas

---
+++ filas/fila/src/main/java/one/digitalinnovation/No.java
@@ -0,0 +1,31 @@
+package one.digitalinnovation;
+
+public class No {
+
+    private Object object;
+    private No refNo;
+
+    public No(){
+
+    }
+
+    public No(Object object){
+        this.refNo = null;
+        this.object = object;
+    }
+
+    public No getRefNo() {
+        return refNo;
+    }
+
+    public void setRefNo(No refNo) {
+        this.refNo = refNo;
+    }
+
+    @java.lang.Override
+    public java.lang.String toString() {
+        return "No{" +
+                "object=" + object +
+                '}';
+    }
+}
